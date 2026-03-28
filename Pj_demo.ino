9f8fad62b213b89c9c44bde0a42bfb2136265387
kien20123 <135220848+kien20123@users.noreply.github.com>
2026-03-28 15:05:30 +0700

    Add files via upload

--- Pj_demo.ino
+++ Pj_demo.ino
@@ -23,7 +23,7 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 bool autoMode = true;
 bool manualPump = false;
-int moisture = 1000;
+int moisture = 1600;
 void setup() {
   Serial.begin(115200);
   pinMode(SOIL_PIN, INPUT);
@@ -32,48 +32,51 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   lcd.init();
   lcd.backlight();
-  Serial.println("Connecting to Blynk...");
+  delay(2000);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  if (Blynk.connected()) {
-    Serial.println("Blynk Connected");
-  }
-  Serial.println("System started");
+  Serial.print("Connecting");
 }
 
-BLYNK_WRITE(V1)
-{
+BLYNK_WRITE(V1) {
   manualPump = param.asInt();
 
   if (!autoMode) {
     digitalWrite(RELAY_PIN, manualPump);
   }
 }
-BLYNK_WRITE(V2)
-{
+BLYNK_WRITE(V2) {
   autoMode = param.asInt();
 
-  if (!autoMode) { 
+  if (!autoMode) {
     digitalWrite(RELAY_PIN, manualPump);
   }
 }
 
 void sendSensor() {
-  int soilState = analogRead(SOIL_PIN);
+  int soilValue = analogRead(SOIL_PIN);
+  Serial.print(soilValue);
+  int maxValue= 2000;
+  int dryValue = 1480;
+  int wetValue = 1220;
+  int minValue= 700;
+  int moisturePercent = map(soilValue, maxValue, minValue, 0, 100);
+  moisturePercent = constrain(moisturePercent, 0, 100);
   int lightValue = digitalRead(LIGHT_PIN);
   lcd.setCursor(0, 0);
   lcd.print("Moisture:");
   lcd.setCursor(10, 0);
   lcd.print("       ");
   lcd.setCursor(10, 0);
-  lcd.print(soilState);
+  lcd.print(moisturePercent);
+  lcd.print("%   ");
   if (lightValue == 1) {
     lcd.setCursor(0, 1);
     lcd.print("Time: Night");
     digitalWrite(LED_PIN, HIGH);
     if (autoMode) {
-      if (soilState > moisture) {
+      if (soilValue > dryValue) {
         digitalWrite(RELAY_PIN, HIGH);  // bật bơm
-      } else {
+      } else if(soilValue < wetValue){
         digitalWrite(RELAY_PIN, LOW);  // tắt bơm
       }
     }
@@ -86,12 +89,17 @@ void sendSensor() {
       digitalWrite(RELAY_PIN, LOW);  // tắt bơm
     }
   }
-  Blynk.virtualWrite(V0, soilState);
+  Blynk.virtualWrite(V0, moisturePercent);
   Blynk.virtualWrite(V3, lightValue);
+  delay(1000);
 }
 void loop() {
 
   Blynk.run();
+  if (Blynk.connected()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Can't connect");
+  }
   sendSensor();
-  delay(100);
 }
